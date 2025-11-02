eed72a916e7155132c8921218d073b6a05d1795a
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-11-02 08:30:18 +0100

    add base settings for app librry_class

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("library.urls"),)
+    path("api/", include("library.urls")),
+    path("apis/", include("library_class.urls")),
 ]
