bae4a3192a3c78f8c53b8a945aa50102b5ccf851
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-11-04 11:26:33 +0100

    Start lerning Authentication

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    'rest_framework.authtoken',
     "library.apps.LibraryConfig",
     "library_class.apps.LibraryClassConfig",
     'library_generic.apps.LibraryGenericConfig',
@@ -130,3 +131,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
